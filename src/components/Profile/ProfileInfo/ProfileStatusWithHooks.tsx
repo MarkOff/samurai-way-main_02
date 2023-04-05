f675863469bb98f7e992a5d73c8809887f176fdb
MarkoVVV <sam.samlin@mail.ru>
2023-04-05 19:18:42 +0300

    1-85 Done! use 'useEffect' and set max symbols in status

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,9 +1,14 @@
-import React, {ChangeEvent, FC, useState} from 'react';
+import React, {ChangeEvent, FC, useEffect, useState} from 'react';
 import {UsersProfilePropsType} from '../ProfileContainer';
 
 export const ProfileStatusWithHooks: FC<UsersProfilePropsType> = ({status, updateStatus}) => {
     const [editMode, setEditMode] = useState(false)
     const [newStatus, setNewStatus] = useState(status)
+
+    useEffect(() => {
+        setNewStatus(status)
+    },[status])
+
 
     const onActiveEditMode = () => {
         setEditMode(true)
@@ -32,7 +37,7 @@ export const ProfileStatusWithHooks: FC<UsersProfilePropsType> = ({status, updat
             {editMode &&
                 <div>
                     <input onChange={onStatusChange} autoFocus={true} onBlur={onDeactivateEditeMode}
-                           value={newStatus} placeholder={'change you status'}/>
+                           value={newStatus} placeholder={'change you status'} minLength={0} maxLength={30}/>
                 </div>
             }
         </div>
